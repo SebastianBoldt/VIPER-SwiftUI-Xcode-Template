9c2504cfa160a055a6a1d1773e24b26522e1ee65
Sebastian Boldt <self.dealloc@googlemail.com>
2019-11-03 19:04:02 +0100

    Fixed some typos

--- VIPER+SwiftUI.xctemplate/___FILEBASENAME___View.swift
+++ VIPER+SwiftUI.xctemplate/___FILEBASENAME___View.swift
@@ -14,14 +14,14 @@ protocol ___VARIABLE_MODULENAME___ViewProtocol {
 
 struct ___VARIABLE_MODULENAME___View: View, ___VARIABLE_MODULENAME___ViewProtocol {
     @ObservedObject private var presenter: ___VARIABLE_MODULENAME___Presenter
-    private let dependencies: ___VARIABLE_MODULENAME___DependenciesProtocol
+    private let dependencies: ___VARIABLE_MODULENAME___ViewDependenciesProtocol
     
     var body: some View {
         Text(presenter.exampleProperty)
     }
     
     init(presenter: ___VARIABLE_MODULENAME___Presenter,
-         dependencies: ___VARIABLE_MODULENAME___DependenciesProtocol) {
+         dependencies: ___VARIABLE_MODULENAME___ViewDependenciesProtocol) {
         self.presenter = presenter
         self.dependencies = dependencies
     }
--- VIPER+SwiftUI.xctemplate/___FILEBASENAME___WireFrame.swift
+++ VIPER+SwiftUI.xctemplate/___FILEBASENAME___WireFrame.swift
@@ -20,8 +20,8 @@ struct ___VARIABLE_MODULENAME___WireFrame: ___VARIABLE_MODULENAME___WireFramePro
         let presenterDependencies = ___VARIABLE_MODULENAME___PresenterDepenencies()
         let presenter = ___VARIABLE_MODULENAME___Presenter(dependencies: presenterDependencies, interactor: interactor)
         
-        let viewDependencies = ___VARIABLE_MODULENAME___ViewDepenencies()
-        let view = ___VARIABLE_MODULENAME___View(presenter: presenter, dependencies: dependencies)
+        let viewDependencies = ___VARIABLE_MODULENAME___ViewDependencies()
+        let view = ___VARIABLE_MODULENAME___View(presenter: presenter, dependencies: viewDependencies)
         
         return AnyView(view)
     }
